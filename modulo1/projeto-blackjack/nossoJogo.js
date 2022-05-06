376718640d95d1cf48b57873207444d735e62396
Diandrey Cristiano Ecker <diandreyecker@hotmail.com>
2022-05-06 17:34:00 -0300

    Projeto BlackJack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,3 +10,46 @@
  * 
  * 
  */
+
+   console.log('Bem-vindo ao jogo BlackJack!')
+
+if (confirm('Quer começar o jogo?') === false){
+   console.log('O jogo acabou')
+
+}else{
+
+   // Cartas do Ususario
+
+   const cartaUsuario1 = comprarCarta()
+   const cartaUsuario2 = comprarCarta()
+   const textoCartaUsusario1 = cartaUsuario1.texto
+   const textoCartaUsusario2 = cartaUsuario2.texto
+   const valorCartasUsuario1 = cartaUsuario1.valor
+   const valorCartasUsuario2 = cartaUsuario2.valor
+   const somaValoresUsuario = valorCartasUsuario1 + valorCartasUsuario2
+
+   // Cartas do Computador
+
+   const cartaComputador1 = comprarCarta()
+   const cartaComputador2 = comprarCarta()
+   const textoCartaComputador1 = cartaComputador1.texto
+   const textoCartaComputador2 = cartaComputador2.texto
+   const valorCartasComputador1 = cartaComputador1.valor
+   const valorCartasComputador2 = cartaComputador2.valor
+   const somaValoresComputador = valorCartasComputador1 + valorCartasComputador2
+
+   // Resultado
+
+   console.log(`Usuário - cartas: ${textoCartaUsusario1} ${textoCartaUsusario2} - pontuação ${somaValoresUsuario}`)
+   console.log(`Computador - cartas: ${textoCartaComputador1} ${textoCartaComputador2} - pontuação ${somaValoresComputador}`)
+
+
+if(somaValoresUsuario > somaValoresComputador){
+      console.log('Você Ganhou!!!')
+}else if(somaValoresUsuario < somaValoresComputador){
+      console.log('Você Perdeu :(')
+}else{
+   console.log('Empate')
+}
+}
+
