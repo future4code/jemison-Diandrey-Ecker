dc898c1927d6fa95a7318aa0e1e09e5a60818ed1
Diandrey Cristiano Ecker <diandreyecker@hotmail.com>
2022-05-10 21:56:21 -0300

    Exercicios Revisao JS

---
+++ modulo1/revisao-js/exercicios.js
@@ -0,0 +1,99 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+   let tamanhoDoArray = array.length
+    return tamanhoDoArray
+}
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+   let arrayInvertido = array.length
+    return arrayInvertido
+}
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  let arrayOrdenado = array.sort();
+    return arrayOrdenado
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+   let numerosPares = array.filter((num)=> {
+       return num % 2 === true
+   })
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+    let numerosPares2 = array.filter((num2)=> {
+        return num2 % 2 === true
+    }).map((num)=>{
+        return {...num2, numerosPares2 ** 2}
+    })
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  let maiorValor = math.max(...array)
+
+  return maiorValor
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+    
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
