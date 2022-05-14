457953c48b89b16c8448b5ee92e42adc1e6909e8
Diandrey Cristiano Ecker <diandreyecker@hotmail.com>
2022-05-13 22:09:42 -0300

    Revisao JS

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -3,48 +3,48 @@
 //    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
 
 
-// EXERCÍCIO 01 - OK
+// EXERCÍCIO 01 
 function retornaTamanhoArray(array){
-//    let tamanhoDoArray = array.length
+   let tamanhoDoArray = array.length
 
-//     return tamanhoDoArray
+    return tamanhoDoArray
 }
 
-// EXERCÍCIO 02 - OK
+// EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-//    let arrayInvertido = array.reverse()
+   let arrayInvertido = array.reverse()
 
-//     return arrayInvertido
+    return arrayInvertido
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-//   let arrayOrdenado = array.sort((a,b)=> a - b)
-//     return arrayOrdenado
+  let arrayOrdenado = array.sort((a,b)=> a - b)
+    return arrayOrdenado
 }
 
-// EXERCÍCIO 04 - OK
+// EXERCÍCIO 04
 function retornaNumerosPares(array) {
-//    let numerosPares = array.filter(pares => (pares % 2) == 0)
-//        return numerosPares
+   let numerosPares = array.filter(pares => (pares % 2) == 0)
+       return numerosPares
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
-    // let numerosPares = array.filter(pares => (pares % 2) == 0)   
-    //     for (let i of numerosPares){
-    //         let numerosParesElevados = []
-    //         numerosParesElevados.push([i]**2)
+    let numerosPares = array.filter(pares => (pares % 2) == 0)   
+        for (let i of numerosPares){
+            let numerosParesElevados = []
+            numerosParesElevados.push([i]**2)
 
-    //         return numerosParesElevados
-    // }
+            return numerosParesElevados
+    }
 }
 
-// EXERCÍCIO 06 - OK
+// EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-//   let maiorValor = Math.max(...array)
+  let maiorValor = Math.max(...array)
 
-//   return maiorValor
+  return maiorValor
 }
 
 // EXERCÍCIO 07
@@ -54,46 +54,63 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+  
 }
 
-// EXERCÍCIO 09 - AINDA NAO FUNCIONA
+// EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-    // if (ladoA === ladoB === ladoC){
-    //     return 'Equilátero'
-    // }else if (ladoA !== ladoB !== ladoC){
-    //     return 'Escaleno'
-    // }else{
-    //     return 'Isóceles'
-    // }
+    if (ladoA === ladoB === ladoC){
+        return 'Equilátero'
+    }else if (ladoA !== ladoB !== ladoC){
+        return 'Escaleno'
+    }else{
+        return 'Isóceles'
+    }
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
+  let novoArray = []
+  array.sort((a,b) => a-b)
+  novoArray.push(array[array.length-2])
+  novoArray.push(array[1])
   
+    return novoArray
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+   let nomeDoFilme = filme.nome
+   let anoDoFilme = filme.ano
+   let diretorDofilme = filme.diretor
+   let atoresDoFilme = [filme.atores.join(', ')]
+
+    return `Venha assistir ao filme ${nomeDoFilme}, de ${anoDoFilme}, dirigido por ${diretorDofilme} e estrelado por ${atoresDoFilme}. `
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+   let pessoaAnonima = {
+       ...pessoa,
+       nome: 'ANÔNIMO'
+   }
+        return pessoaAnonima
 }
 
-// EXERCÍCIO 13A - OK
+// EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-//    return pessoas.filter(pessoa =>{
-//        return (pessoa.idade <= 60 && pessoa.idade > 14 && pessoa.altura >= 1.5)
+   return pessoas.filter(pessoa =>{
+       return (pessoa.idade <= 60 && pessoa.idade > 14 && pessoa.altura >= 1.5)
 
-//    })
+   })
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+     return pessoas.filter(pessoa =>{
+       return ((pessoa.idade > 60 || pessoa.idade < 14 ) || pessoa.altura < 1.5)
+
+   })
 }
 
 // EXERCÍCIO 14
