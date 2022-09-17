6a14ee0ff9c7384864ea91c816e57381804f33fb
Diandrey <diandreyecker@hotmail.com>
2022-09-16 23:24:43 -0300

    Labex - Custom Hook

--- React-Pre-Back/labex2/src/pages/ListTripsPage.js
+++ React-Pre-Back/labex2/src/pages/ListTripsPage.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useRequestData } from '../hook/useRequestData';
+import { BASE_URL } from '../constants/constants';
 
 const ListTripsPage = () => {
   const navigate = useNavigate();
+
+  const [data, isLoadingTrips, errorTrips] = useRequestData(`${BASE_URL}trips`)
+
+  const listaDeData = data && data.trips && data.trips.map((viagem) => {
+    return ( 
+    <div key = {viagem.id}>
+      <p> {viagem.name} </p>
+    </div>
+    
+  )})
+
   return (
-    <div>
-      <h1>List Trips Page</h1>
+    
+      <>
+        {isLoadingTrips && <p> Carregando data</p>}
+        {!isLoadingTrips && errorTrips && <p> Ocorreu um erro </p>}
+        {!isLoadingTrips && data && data.trips && data.trips.length > 0 && listaDeData}
+        {!isLoadingTrips && data && data.trips && data.trips.length === 0 && <p> Não há viagens </p>}
+
+        <h1>List Trips Page</h1>
       <button onClick={() => navigate('/application')}>Inscrever-se</button>
       <button onClick={() => navigate(-1)}>Voltar</button>
-    </div>
+      </>
   )
 }
 
