5d0e80c643371adbe5c99a33171b3f384d839c2d
Diandrey <diandreyecker@hotmail.com>
2022-10-21 23:31:50 -0300

    Fix: Lisa Typescript

--- modulo-5/lista-typescript/src/exercicio4.ts
+++ modulo-5/lista-typescript/src/exercicio4.ts
@@ -1,32 +1,31 @@
 
-// enum Cargos {
-//     MARKETING = "marketing",
-//     VENDAS = "vendas",
-//     FINANCEIRO = "financeiro"
-// }
+enum Cargos {
+    MARKETING = "marketing",
+    VENDAS = "vendas",
+    FINANCEIRO = "financeiro"
+}
 
-// type Colaboradores = {
-//     nome: string,
-//     salario: Number,
-//     setor: Cargos,
-//     presencial: boolean
-// }
+type Colaboradores = [{
+    nome: string,
+    salario: Number,
+    setor: Cargos,
+    presencial: boolean
+}]
 
-// type ListaColaboradores = [
-// 	{ nome: "Marcos", salário: 2500, setor: Cargos.MARKETING, presencial: true },
-// 	{ nome: "Maria" ,salário: 1500, setor: Cargos.VENDAS, presencial: false},
-// 	{ nome: "Salete" ,salário: 2200, setor: Cargos.FINANCEIRO, presencial: true},
-// 	{ nome: "João" ,salário: 2800, setor: Cargos.MARKETING, presencial: false},
-// 	{ nome: "Josué" ,salário: 5500, setor: Cargos.FINANCEIRO, presencial: true},
-// 	{ nome: "Natalia" ,salário: 4700, setor: Cargos.VENDAS, presencial: true},
-// 	{ nome: "Paola" ,salário: 3500, setor: Cargos.MARKETING, presencial: true}
-// ]
+const listaColaboradores = [
+	{ nome: "Marcos", salário: 2500, setor: Cargos.MARKETING, presencial: true },
+	{ nome: "Maria" ,salário: 1500, setor: Cargos.VENDAS, presencial: false},
+	{ nome: "Salete" ,salário: 2200, setor: Cargos.FINANCEIRO, presencial: true},
+	{ nome: "João" ,salário: 2800, setor: Cargos.MARKETING, presencial: false},
+	{ nome: "Josué" ,salário: 5500, setor: Cargos.FINANCEIRO, presencial: true},
+	{ nome: "Natalia" ,salário: 4700, setor: Cargos.VENDAS, presencial: true},
+	{ nome: "Paola" ,salário: 3500, setor: Cargos.MARKETING, presencial: true}
+]
 
-// function buscarColaborador (colaborador: ListaColaboradores[], setor, presencial): ListaColaboradores[] {
-       
-//             return colaborador.filter (
-//                 (funcionario: ListaColaboradores) => {
-//                     return 
-//                 }
-//             )
-// }
+function buscarColaborador ([]) {
+
+            return listaColaboradores.filter
+                (funcionario => funcionario.setor === Cargos.MARKETING && funcionario.presencial === true)
+}
+
+console.log(buscarColaborador([]))
--- modulo-5/lista-typescript/src/exercicio5.ts
+++ modulo-5/lista-typescript/src/exercicio5.ts
@@ -1,4 +1,10 @@
-// [
+// type Usuarios = [{
+//     name: string,
+//     email: string,
+//     role: string
+// }]
+
+// const listaDeUsuarios = [
 // 	{name: "Rogério", email: "roger@email.com", role: "user"},
 // 	{name: "Ademir", email: "ademir@email.com", role: "admin"},
 // 	{name: "Aline", email: "aline@email.com", role: "user"},
@@ -6,3 +12,14 @@
 // 	{name: "Adilson", email: "adilson@email.com", role: "user"},  
 // 	{name: "Carina", email: "carina@email.com", role: "admin"}      
 // ] 
+
+// function recebeEmail ([]): string {
+    
+//     const adminUser = listaDeUsuarios.map( item => {
+//         const container = [{}];
+
+//         container[item.role]
+//     })
+    
+//     return
+// }
